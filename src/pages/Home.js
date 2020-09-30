781deaab3298c115e41f3ce67d7c374e60eb2b41
Jalen Halverson <Jhalvey08@gmail.com>
2020-09-29 19:56:13 -0700

    Updating packages and cleaning things up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import {
-  Typography,
+    Typography,
+    withStyles,
 } from '@material-ui/core';
 
-export default () => (
-  <Typography variant="h4">Big Macs!</Typography>
+
+const styles = theme => ({
+    main: {
+        padding: theme.spacing(3),
+        [theme.breakpoints.down('xs')]: {
+            padding: theme.spacing(2),
+        },
+    },
+});
+
+const Home = ({ classes }) => (
+    <Typography variant="h4">Big Macs!</Typography>
 );
+
+export default withStyles(styles)(Home);
